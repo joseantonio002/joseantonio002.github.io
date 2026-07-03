e8c89703540d0316bbabb0a8bc1dd9d4cffd97d5
Jose <jantunezpulido@gmail.com>
2026-07-03 20:53:39 +0100

    landing

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -72,7 +72,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: '',
-        text: "Multidisciplinary engineer with a strong interest in understanding systems at a deep level. I usually work with data or build end-to-end applications. I stay up to date with the latest technologies and have recently been incorporating agentic engineering into my development process.\n\nI have worked at TITSA as a Data Scientist and recently completed a master's degree in Data Engineering. Some of the projects I have built on my own include an OSINT platform to compile and visualize data from heterogeneous sources, a desktop app to find historical and current chart patterns (this one was before AI), and a dashboard for air transport statistics in the Canary Islands.",
+        text: "I have experience working with data and building end to end applications. Some of the projects I have built on my own include an OSINT platform to compile and visualize data from heterogeneous sources, a dashboard for air transport statistics in the Canary Islands and a desktop app to find historical and current chart patterns.\n\nI have worked at TITSA as a Data Scientist and recently completed a master’s degree in Data Engineering, where I gained experience in large-scale data processing, database management, and related data engineering/data science practices. Currently incorporating agentic engineering into my workflow by building a project (coming soon)",
         actions: [
             {
                 text: 'See my projects',
